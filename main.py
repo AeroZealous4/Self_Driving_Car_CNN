8a6f3e3c6952f29436c8b0e6646e0091aec76c42
AeroZealous4 <akshay.pampatwar5@gmail.com>
2019-11-17 14:12:02 +0530

    Use this file for calling main functions

---
+++ main.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils import data
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+import cv2
+import numpy as np
+import csv
+
+import controller 
+#Data Aquisition 
+
+#Preprocessing
+
+#Creating Model "controller.py" (Architecture, Corresponding Code etc)
+
+#Training (Generalized for model in "controller.py")
+
+#Generate h5 file "controller.h5" which have trained model weights
+
+#Create seperate file "drive.py" which will use "controller.py" & 
